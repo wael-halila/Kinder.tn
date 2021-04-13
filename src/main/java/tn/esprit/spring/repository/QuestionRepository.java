2ab03fdbd2b4702cf8dc6eb6e21a30ae2f1959fe
Salma9507 <79111940+Salma9507@users.noreply.github.com>
2021-04-13 05:11:40 +0100

    Add files via upload

---
+++ src/main/java/tn/esprit/spring/repository/QuestionRepository.java
@@ -0,0 +1,18 @@
+package tn.esprit.spring.repository;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+import tn.esprit.spring.entity.Question;
+
+
+
+
+public interface QuestionRepository extends CrudRepository<Question, Integer>{
+
+
+
+}
