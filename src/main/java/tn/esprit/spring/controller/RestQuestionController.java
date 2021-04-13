f26aa735b3f3d682b939df3255ba8163527cf0a1
wael-halila <79110082+wael-halila@users.noreply.github.com>
2021-04-13 10:47:16 +0100

    path director
    /Question

--- src/main/java/tn/esprit/spring/controller/RestQuestionController.java
+++ src/main/java/tn/esprit/spring/controller/RestQuestionController.java
@@ -1,11 +1,12 @@
 package tn.esprit.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,9 @@ import tn.esprit.spring.sevice.impl.QuestionServiceImpl;
 
 
 
+@CrossOrigin("*")
 @RestController
+@RequestMapping("/Question")
 public class RestQuestionController {
 
 	@Autowired
